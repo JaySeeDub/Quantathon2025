1be0920ca4c92ba48123fabcee30e46b3cfcd3b9
Andrew Maciejunes <Andrewmaciejunes@yahoo.com>
2025-10-21 15:23:19 -0400

    Added QKE

--- demo/utils/Helper.py
+++ demo/utils/Helper.py
@@ -42,3 +42,12 @@ def plot_confusion_matrix_percent(cm, labels, title):
     plt.title(title)
     plt.tight_layout()
     plt.show()
+    
+def removeWeakTornados(df_labels, df_data):
+    #Removes EF0 and EF1 tornadoes
+    mask_remove = df_labels.isin([0, 1])
+    removed_indices = df_labels.index[mask_remove]
+    df_labels = df_labels.loc[~mask_remove].reset_index(drop=True)
+    df_data = df_data.drop(index=removed_indices).reset_index(drop=True)
+
+    return df_labels, df_data
